9706d74dcf2ab3636e198a98bfa2b9a4cb4ae18f
KuuShyn <magalonakaj@gmail.com>
2023-11-11 22:11:47 +0800

    gawgwa

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -o errexit
 
-pip install -r requirements.txt
+pip install Django
+pip install deepface
+pip install psycopg2-binary
 
 python manage.py collectstatic --noinput
 python manage.py migrate
